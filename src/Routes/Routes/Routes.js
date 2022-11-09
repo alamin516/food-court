4fa29175f0fd3a06ee005f2d9ebd1d4fe8fbd6bf
Md Alamin <alaminduc516@gmail.com>
2022-11-09 12:53:00 +0600

    Routes added

---
+++ src/Routes/Routes/Routes.js
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const routes = createBrowserRouter([
+    {
+        path: '/', 
+        element: <h2>Hello</h2>
+    }
+])
+
+export default routes;
